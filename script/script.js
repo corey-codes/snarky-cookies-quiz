b2aa80e41c3d7d00f81ae99ef8109ba7f18659d6
hellcat04 <coreyderouin@outlook.com>
2019-11-14 17:14:51 -0500

    first commit :point_up:

---
+++ script/script.js
@@ -0,0 +1,84 @@
+
+// GENERAL GAME VARIABLES ===============================
+
+const currentQuestion = {};
+const questions;
+const options;
+let score = 0;  
+
+
+
+// QUESTIONS ARRAY
+
+const quiz = [
+    {
+    question: 'question here',
+    answer1: 'answer1 here',
+    answer2: 'answer2 here',
+    answer3: 'answer3 here',
+    answer4: 'answer4 here'
+    },
+
+    {
+        question: 'question here',
+        answer1: 'answer1 here',
+        answer2: 'answer2 here',
+        answer3: 'answer3 here',
+        answer4: 'answer4 here'
+    },
+
+    {
+        question: 'question here',
+        answer1: 'answer1 here',
+        answer2: 'answer2 here',
+        answer3: 'answer3 here',
+        answer4: 'answer4 here'
+    },
+
+    {
+        question: 'question here',
+        answer1: 'answer1 here',
+        answer2: 'answer2 here',
+        answer3: 'answer3 here',
+        answer4: 'answer4 here'
+    },
+
+    {
+        question: 'question here',
+        answer1: 'answer1 here',
+        answer2: 'answer2 here',
+        answer3: 'answer3 here',
+        answer4: 'answer4 here'
+    },
+    {
+        question: 'question here',
+        answer1: 'answer1 here',
+        answer2: 'answer2 here',
+        answer3: 'answer3 here',
+        answer4: 'answer4 here'
+    },
+
+    {
+        question: 'question here',
+        answer1: 'answer1 here',
+        answer2: 'answer2 here',
+        answer3: 'answer3 here',
+        answer4: 'answer4 here'
+    },
+]
+        
+
+
+
+// DOC READY ========================
+
+$(document).ready(function(){
+    // console.log('ready!');
+
+
+// START GAME ========================
+   
+    
+
+// ===================================   
+}); //END OF DOC READY
